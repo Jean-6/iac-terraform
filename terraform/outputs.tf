05e540825485212401cfe12f292e56ec81d5f4c2
Jean <oka.jeanjaures@gmail.com>
2025-12-04 21:06:24 +0100

    to define output values of infra profect

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,19 @@
 # To expose useful informations after deployment
 
-output "vpc_id" {
-  description = "ID of VPC created"
-  value = "aws_vpc.main.id"
+#output "vpc_id" {
+#  description = "ID of VPC created"
+#  value = "aws_vpc.main.id"
+#}
+
+#output "ecs_cluster_ name" {
+#  description = "Name of ECS cluster"
+#  value = "aws_ecs_cluster.app.name"
+#}
+
+output "alb_dns" {
+  value = aws_lb.alb.dns_name
 }
 
-output "ecs_cluster_ name" {
-  description = "Name of ECS cluster"
-  value = "aws_ecs_cluster.app.name"
+output "ecr_repo_url" {
+  value = aws_ecr_repository.app.repository_url
 }
