35fa66065d86e67a76a27529da7d40d4214feb16
Jean <oka.jeanjaures@gmail.com>
2025-12-04 20:01:11 +0100

    add news variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,21 @@ variable "aws_region" {
   default = "eu-west-1"
 }
 
+
+variable "account_id" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+  default = "vegnbio-api"
+}
+
+variable "image_tag" {
+  type = string
+  default = "1.0.0"
+}
+
 variable "aws_profile" {
     description = "AWS CLI Profile to use"
     type = string
@@ -16,4 +31,17 @@ variable "vpc_cidr" {
   description = "CIDR du VPC"
   type = string
   default = "10.0.0.0/16"
+}
+
+#
+variable "vpc_id" { # from network.tf ou outputs.tf
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
 }
