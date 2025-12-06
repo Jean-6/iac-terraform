7a2c4aaf810c9e76ceb30cccb9b3d660ac29e9ed
Jean <oka.jeanjaures@gmail.com>
2025-12-06 15:51:46 +0100

    change cloud region (irlande -> paris) for subnet

--- terraform/network.tf
+++ terraform/network.tf
@@ -4,7 +4,6 @@ resource "aws_vpc" "main" {
     Name = "main-vpc"
   }
 }
-# VPC
 
 # Internet Gateway for public subnets
 resource "aws_internet_gateway" "igw" {
@@ -22,7 +21,6 @@ resource "aws_route_table" "public" {
   tags = { Name = "public-rt" }
 }
 
-
 # Association route table -> subnets publics
 resource "aws_route_table_association" "public_1" {
   subnet_id      = aws_subnet.public_1.id
@@ -34,11 +32,12 @@ resource "aws_route_table_association" "public_2" {
   route_table_id = aws_route_table.public.id
 }
 
+# VPC
 # Subnets public (pour ECS tasks)
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.5.0/24"
-  availability_zone = "eu-west-1a"
+  availability_zone = "eu-west-3a"
   map_public_ip_on_launch = true
   tags = { Name = "public-subnet-1" }
 }
@@ -46,7 +45,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.6.0/24"
-  availability_zone = "eu-west-1b"
+  availability_zone = "eu-west-3b"
   map_public_ip_on_launch = true
   tags = { Name = "public-subnet-2" }
 }
@@ -56,7 +55,7 @@ resource "aws_subnet" "public_2" {
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "eu-west-1a"
+  availability_zone = "eu-west-3a"
 
   tags = {
     Name = "private-subnet-1"
@@ -66,7 +65,7 @@ resource "aws_subnet" "private_1" {
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "eu-west-1b"
+  availability_zone = "eu-west-3b"
 
   tags = {
     Name = "private-subnet-2"
