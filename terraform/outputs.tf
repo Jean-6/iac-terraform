76a370f2cc7638f790c0edb96373d39191745077
Jean <oka.jeanjaures@gmail.com>
2025-12-06 15:52:38 +0100

    output variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,10 +10,16 @@
 #  value = "aws_ecs_cluster.app.name"
 #}
 
-output "alb_dns" {
-  value = aws_lb.alb.dns_name
-}
+
 
 output "ecr_repo_url" {
   value = aws_ecr_repository.app.repository_url
+}
+
+output "alb_arn" {
+  value = aws_lb.alb.arn
+}
+
+output "alb_dns" {
+  value = aws_lb.alb.dns_name
 }
